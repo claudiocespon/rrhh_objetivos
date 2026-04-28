7c4fad79e9186b9358f3c2324cfa7b48202f192c
cdcespon permaquim <110120021+claudiocespon@users.noreply.github.com>
2026-04-28 11:02:12 -0300

    chore: agregar migración para AreaEspecificaId y PorcentajeArea en Objetivo

---
+++ Objetivos.Web/Migrations/20260428135626_AddAreaEspecificaToObjetivo.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Objetivos.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAreaEspecificaToObjetivo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AreaEspecificaId",
+                table: "Objetivos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PorcentajeArea",
+                table: "Objetivos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Objetivos_AreaEspecificaId",
+                table: "Objetivos",
+                column: "AreaEspecificaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Objetivos_Areas_AreaEspecificaId",
+                table: "Objetivos",
+                column: "AreaEspecificaId",
+                principalTable: "Areas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Objetivos_Areas_AreaEspecificaId",
+                table: "Objetivos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Objetivos_AreaEspecificaId",
+                table: "Objetivos");
+
+            migrationBuilder.DropColumn(
+                name: "AreaEspecificaId",
+                table: "Objetivos");
+
+            migrationBuilder.DropColumn(
+                name: "PorcentajeArea",
+                table: "Objetivos");
+        }
+    }
+}
